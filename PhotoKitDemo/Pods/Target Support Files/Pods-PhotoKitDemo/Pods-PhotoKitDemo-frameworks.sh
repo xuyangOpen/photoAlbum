cceb11abab3c7a6974281ccd0b0e298cf2aa3bcf
xuyang <xuyang767660732@163.com>
2017-01-04 16:00:11 +0800

    修改了使用的库

--- PhotoKitDemo/Pods/Target Support Files/Pods-PhotoKitDemo/Pods-PhotoKitDemo-frameworks.sh
+++ PhotoKitDemo/Pods/Target Support Files/Pods-PhotoKitDemo/Pods-PhotoKitDemo-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TZImagePickerController/TZImagePickerController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/TZImagePickerController/TZImagePickerController.framework"
 fi
